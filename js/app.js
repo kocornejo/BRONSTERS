ca76a0e4c0a4146c6edb5677096e960c2b101f87
kocornejo <kocornejo@gmail.com>
2022-07-28 22:24:11 -0700

    i think i did it...

--- js/app.js
+++ js/app.js
@@ -1,98 +1,161 @@
-console.log('check');
-// const monsterList = {
-//     dragon:  'imgs/dragon.webp',
-//     worm: 'imgs/dragon.webp',
-//     demon:'imgs/demon.jpeg',
-// }
+//
+//Constants for buttons
+const petBtn = document.querySelector("#action-pet");
+const feedBtn = document.querySelector("#action-feed");
+const trainBtn = document.querySelector("#action-train");
+const startBtn = document.querySelector("#action-menu-start-game");
+const replayBtn = document.querySelector("#action-replay");
 
-// const MONSTERS = [
-//     "dragon",
-//     "worm",
-//     "demon",
-// ];
-const dungeon = 'imgs/cage.jpeg';
+//music/sound effects
+const music=new Audio('js/music/cave_theme_2.wav');
+// music.play();
+music.loop =true;
+music.playbackRate = 2;
+music.pause();
+music.volume=0.2;
 
-const monsterList = {
-    dragon: {
-        imageUrl: 'imgs/dragon.webp'
-    },
-    worm: {
-        imageUrl: 'imgs/dragon.webp'
-    },
-    demon: {
-        imageUrl: 'imgs/demon.jpeg'
-    }
+const click_sound=new Audio('js/music/Blip_select 67.wav');
+click_sound.volume=0.1;
+
+const victory=new Audio('js/music/Fanfare_1.wav');
+victory.volume=0.1;
+
+const gameOver=new Audio('js/music/Game_Over_2.wav');
+gameOver.volume=0.1;
+
+//Constants for main bar
+const happinessHp = document.querySelector("#happiness-hp");
+const hungerHp = document.querySelector("#hunger-hp");
+const disciplineHp = document.querySelector("#discipline-hp");
+const daysBar = document.querySelector("#days");
+
+//Game settings
+const maxHappiness = 300;
+const maxHunger = 300;
+const maxDiscipline = 300;
+//Game speed
+let day = 20;
+
+//New object
+function Monster() {
+  this.happiness = maxHappiness;
+  this.hunger = maxHunger;
+  this.discipline = maxDiscipline;
 }
 
-//[
-//  "imgs/dragon.webp",
-//  "imgs/Regenwurm1.jpeg",
-//  "imgs/demon.jpeg"
-// ];
-// const cage = (css/imgs/cage.jpeg);
+//Abilities
+Monster.prototype.actionPet = function() {
+    this.happiness+=40 / (day * 2)
+};
 
-let randomMonster; // will pick a random monster from MONSTERS constant
-let score; // score against monster
-let winner; // depleting health bar wins
-let start;
+Monster.prototype.actionEat = function() {
+	this.hunger+=120 / (day * 2)
+};
 
-const scoreEls = {
-    player: document.querySelector('#score'),
-}
-const startEl = {
-    imgEl: document.querySelector('#dungeon > img')
-}
-// cache DOM elements
-const healthBar = document.querySelector('#health');
-const dungeonEl = document.querySelector('#dungeon');
-const commandBtns = document.querySelector('#commands');
-const startBtn = document.querySelector('#start');
-const replayBtn = document.querySelector('#replay');
+Monster.prototype.actionTrain = function() {
+	this.discipline+=80 / (day * 2)
+};
 
+Monster.prototype.tick = function() {
+    this.happiness--;
+    this.hunger-=8;
+    this.discipline-=8;
+};
 
-// dungeonEl.setAttribute("img",'imgs/demon.jpeg' )
-// startBtn.addEventListener('click', startGame);
-commandBtns.addEventListener('click', attack);
+let mnstr = new Monster();
+let happinessHpCount;
+let hungerHpCount;
+let disciplineHpCount;
+let days = 0;
 
-replayBtn.addEventListener('click', init);
+//Controllers
+petBtn.addEventListener("click", function() {
+	mnstr.actionPet(), click_sound.play();
+});
 
-init()
+feedBtn.addEventListener("click", function() {
+	mnstr.actionEat(), click_sound.play();
+});
 
-function init(){
-    // console.log(e.target, e.target.tagName)
-    // if(e.target.tagname === 'start') return
+trainBtn.addEventListener("click", function() {
+	mnstr.actionTrain(), click_sound.play();
+});
 
-    //not actually what i wanted here
-    const randomIndex = Math.floor(Math.random() * monsterList.length);
-   randomMonster = monsterList[randomIndex]
+replayBtn.addEventListener("click", function() {
+    startGame(), click_sound.play();
+});
 
-score = {
-    player: 0,
-}
-
-start = {
-    dungeon: 'cage'
-}
-
-render()
- }
-
-function attack(e){
-    console.log(e.target, e.target.tagName)
-
-    if(e.target.tagName === 'section') return; // testing buttons
-
-    score = {
-        player: 0,
-    }
+startBtn.addEventListener("click", function() {
+	startGame(), click_sound.play(), music.play();
+});
 
 
-    render()
+
+
+
+//Togglers for buttons
+document.querySelector(".game-screen").classList.toggle("hide");
+
+function MainMenu() {
+	document.querySelector(".main-menu-screen").classList.toggle("hide");
 }
 
-function render(){
-    scoreEls.player.innerText = score.player;
 
-    // let starty = startEl[start].imgEl.src = monsterList[start[starty]].imageUrl
-} 
+function startGame() {
+	document.querySelector(".game-screen").classList.toggle("hide");
+	document.querySelector(".main-menu-screen").classList.toggle("hide");
 
+
+	//Start game
+	core();
+	let coreUpdate = setInterval(core, 100 * day);
+
+	//Main function of monster
+	function core() {
+		//console.log(mnstr);
+		happinessHpCount = (mnstr.happiness / maxHappiness * 100).toFixed(2);
+		hungerHpCount = (mnstr.hunger / maxHunger * 100).toFixed(2);
+		disciplineHpCount = (mnstr.discipline / maxDiscipline * 100).toFixed(2);
+
+		//Scores
+		days++;
+		daysBar.innerHTML = days;
+
+		//Death ability
+		if ((disciplineHpCount <= 0) || (happinessHpCount <= 0) || (hungerHpCount <= 0)) {
+			playHpCount = 0;
+			happinessHpCount = 0;
+			disciplineHpCount = 0;
+			clearInterval(coreUpdate);
+			alert("Days: " + days + "\n  DON'T TALK TO ME OR MY SON EVER AGAIN! ");
+            music.pause();
+            gameOver.play();
+		}
+
+		//Max health percentage (for player)
+		if ((mnstr.happiness / maxHappiness * 100) > 100) {
+			happinessHpCount = 100;
+		}
+		if ((mnstr.hunger / maxHunger * 100) > 100) {
+			hungerHpCount = 100;
+		}
+		if ((mnstr.discipline / maxDiscipline * 100) > 100) {
+			disciplineHpCount = 100;
+		}
+        if (days === 25){
+            music.pause();
+            victory.play();
+            alert("Days survived:" + days + "\n You'll be rewarded greatly");
+        }
+
+		//Show HP on screen
+		happinessHp.innerHTML = happinessHpCount;
+		hungerHp.innerHTML = hungerHpCount;
+		disciplineHp.innerHTML = disciplineHpCount;
+
+		//Remove HP every tick
+		mnstr.tick();
+
+
+	}
+}
